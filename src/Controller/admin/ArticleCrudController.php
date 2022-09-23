59960d1075a3384d526cf9d7443eb7a574c4c396
clemmlec <clement.canale@gmail.com>
2022-09-23 11:50:18 +0200

    fix: front article & admin ckeditor pour article

--- src/Controller/admin/ArticleCrudController.php
+++ src/Controller/admin/ArticleCrudController.php
@@ -4,11 +4,14 @@ namespace App\Controller\admin;
 
 use App\Entity\Article;
 use App\Controller\admin\TagsCrudController;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -20,12 +23,19 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig')
+        ;
+    }
+    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name'),
-            TextEditorField::new('content'),
+            TextEditorField::new('content')->setFormType(CKEditorType::class),
             AssociationField::new('tags')->setCrudController(TagsCrudController::class),
             AssociationField::new('user')->hideOnForm(),
             DateTimeField::new('created_at')->hideOnForm(),
