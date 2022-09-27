c6e0f27b30ebb7ce8e3be494ea0b217642b9e6ea
clemmlec <clement.canale@gmail.com>
2022-09-27 12:00:47 +0200

    feat: add article like and suggest + component article

--- src/Controller/Front/ArticleController.php
+++ src/Controller/Front/ArticleController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Front;
 
 use App\Entity\Article;
+use App\Entity\ArticleLiked;
+use App\Entity\ArticleSuggestion;
 use App\Repository\ArticleRepository;
+use App\Repository\ArticleLikedRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ArticleSuggestionRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -33,4 +37,50 @@ class ArticleController extends AbstractController
             'articles' => $articles
         ]);
     }
+
+    #[Route('/liked/{id}', name: 'app_article_liked', methods: ['GET'])]
+    public function likedArticle(?Article $article, ArticleLikedRepository $articleFavRepo, ArticleRepository $articleRepo, Security $security)
+    {
+        $articleFav = new ArticleLiked();
+        $follow = $articleRepo->find($article);
+
+        $user = $security->getUser();
+
+        if ($follow && $user) {
+            $dejaAmis = $articleFavRepo->findOneBy(['user' => $user, 'article' => $article]);
+            if (!$dejaAmis) {
+                $articleFav->setUser($user)
+                ->setarticle($follow);
+                $articleFavRepo->add($articleFav, true);
+
+                return new Response('article ajouté', 201);
+            }
+            $articleFavRepo->remove($dejaAmis, true);
+
+            return new Response('article retiré des favoris', 201);
+        }
+
+        return new Response('demande de favoris non valide', 404);
+    }
+
+    #[Route('/suggest/{id}/{message}', name: 'user.article.suggest', methods: ['GET'])]
+    public function signalerarticle(?Article $article, String $message, Security $security, ArticleRepository $artRepo, ArticleSuggestionRepository $artSignalRepo)
+    {
+        $user = $security->getUser();
+
+        if ($article && $user) {
+            
+            $newSignal = new ArticleSuggestion();
+            $newSignal->setUser($user)
+            ->setarticle($article)
+            ->setMessage($message);
+            $artSignalRepo->add($newSignal, true);
+
+            return new Response('article signaler', 201);
+
+        }
+
+        return new Response('article non trouvé', 404);
+    }
+
 }
